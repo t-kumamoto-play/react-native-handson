67a3b5a80c81a9be6fc1a465554e4b65861f8a49
T.Kumamoto <82348027+t-kumamoto-play@users.noreply.github.com>
2024-02-01 12:30:42 +0900

    Navigationで画面遷移 (#1)
    * Navigationで画面遷移
    
    * 画面遷移を作成

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+export default function HomeScreen(props) {
+  const { navigation } = props;
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+      <Button
+        title='Detailに遷移する'
+        onPress={() => {
+          navigation.navigate('Detail');
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
